b6e33dc524d4d328913833e6c329528b20f30597
Richard Everett <richard@codrie.com>
2017-10-23 21:47:39 +1100

    Documentation added

--- Richev.NaiveDiff.Core/ExtensionMethods.cs
+++ Richev.NaiveDiff.Core/ExtensionMethods.cs
@@ -4,6 +4,12 @@ namespace Richev.RegexDiff.Core
 {
     public static class ExtensionMethods
     {
+        /// <summary>
+        /// Adds the object to the end of the <see cref="System.Collections.Generic.Queue{T}"/>, unless the object is null.
+        /// </summary>
+        /// <typeparam name="T">Specifies the type of elements in the queue.</typeparam>
+        /// <param name="queue">The queue.</param>
+        /// <param name="value">The object to add to the <see cref="System.Collections.Generic.Queue{T}"/>.</param>
         public static void EnqueueUnlessNull<T>(this Queue<T> queue, T value) where T : class
         {
             if (value != null)
--- Richev.NaiveDiff.Core/Matcher/ILineMatcher.cs
+++ Richev.NaiveDiff.Core/Matcher/ILineMatcher.cs
@@ -2,6 +2,9 @@
 {
     public interface ILineMatcher
     {
+        /// <summary>
+        /// Returns true if the lines match.
+        /// </summary>
         bool Matches(string lineLeft, string lineRight);
     }
 }
